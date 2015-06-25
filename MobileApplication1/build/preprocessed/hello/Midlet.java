0e2b83fa7348fd59ce8d8c0731d14de6443bd2de
Imas-Siti-Munawaroh <1206061@sttgarut.ac.id>
2015-06-25 11:26:00 +0700

    tambah file

---
+++ MobileApplication1/build/preprocessed/hello/Midlet.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author user
+ */
+public class Midlet extends MIDlet {
+
+    public void startApp() {
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+}
